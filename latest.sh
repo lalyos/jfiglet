d71529814fae59bab8ae77e33bb9d24144cc96ab
lalyos <lalyos@yhoo.com>
2014-04-06 22:47:25 +0200

    display version added to test script

--- latest.sh
+++ latest.sh
@@ -3,6 +3,7 @@
 MSG=${1-mutyi}
 
 LATEST=$(curl -s http://central.maven.org/maven2/com/github/lalyos/jfiglet/maven-metadata.xml|grep "<latest>"| sed "s:</.*::;s:.*>::")
+echo LATEST=$LATEST
 curl -so jfiglet.jar http://central.maven.org/maven2/com/github/lalyos/jfiglet/$LATEST/jfiglet-$LATEST.jar
 java -jar jfiglet.jar $MSG
 rm jfiglet.jar
