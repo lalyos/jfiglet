4e40bb8c2f2934013cc5713b56a98621dd7af9ed
mcwarman <mcwarman@gmail.com>
2015-06-12 15:50:23 +0000

    Add the -o option back to JFiglet which was incorrectly moved in merge

--- src/main/java/com/github/lalyos/jfiglet/JFiglet.java
+++ src/main/java/com/github/lalyos/jfiglet/JFiglet.java
@@ -17,8 +17,10 @@ public class JFiglet {
         FittingRules.OVERRIDE_LAYOUT horizontal = FittingRules.OVERRIDE_LAYOUT.DEFAULT;
         while(arguments.hasNext()){
             String arg = arguments.next();
-            if (arg.equals("-f")){
+            if (arg.equals("-f")) {
                 font = requireNextArgument(arguments, arg);
+            } else if ("-o".equals(arg)) {
+                out = new PrintStream(new FileOutputStream(requireNextArgument(arguments, arg)));
             } else if (arg.equals("-v")){
                 try {
                     vertical = FittingRules.OVERRIDE_LAYOUT.valueOf(requireNextArgument(arguments, arg));
@@ -58,7 +60,7 @@ public class JFiglet {
     private static String usage() {
         final StringWriter result = new StringWriter();
         final PrintWriter pw = new PrintWriter(result);
-        pw.println("Usage: java -jar jfiglet.jar [-f FLF] [-h HORIZONTAL SMUSHING] [-v VERTICAL SMUSHING] MESSAGE");
+        pw.println("Usage: java -jar jfiglet.jar [-f FLF] [-o OUTFILE] [-h HORIZONTAL SMUSHING] [-v VERTICAL SMUSHING] MESSAGE");
         pw.println("Prints MESSAGE to stdout as ASCII art using Figlet font");
         pw.println("Example: java -jar jfiglet.jar -f \"/opt/myfont.flf\" -h \"full_width\" -v \"fitting\" \"Hello World\"");
         pw.println("\n");
@@ -67,6 +69,7 @@ public class JFiglet {
         pw.println("                 When FLF starts with `http://'|`https://' file will be fetched from WWW,");
         pw.println("                 if FLF starts with `classpath:' then it will be looked for in JRE classpath,");
         pw.println("                 otherwise FLF is path to FLF file in file system.");
+        pw.println("  -o  (Optional) Set a file path to output result to.");
         pw.println("  -h  (Optional) Used to override the horizontal smushing of the font, available options:");
         pw.println("                 " + Arrays.toString(FittingRules.OVERRIDE_LAYOUT.values()).toLowerCase());
         pw.println("  -v  (Optional) Used to override the vertical smushing of the font, available options:");
